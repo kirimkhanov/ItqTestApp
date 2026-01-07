14a0231b9bde3e93b6b06e3dc92c7465b1bd8ee9
Kerimhanov, Shahmurad (RU - External) <Shahmurad.Kerimhanov@externals.hilti.com>
2026-01-07 16:15:18 +0300

    Added domain layer

---
+++ ITQTestApp.Domain/Entities/CodeValueItem.cs
@@ -0,0 +1,24 @@
+﻿using ITQTestApp.Domain.ValueObjects;
+
+namespace ITQTestApp.Domain.Entities
+{
+    public class CodeValueItem
+    {
+        public int Id { get; private set; }
+
+        public Code Code { get; private set; }
+
+        public string Value { get; private set; }
+
+        private CodeValueItem() { }
+
+        public CodeValueItem(Code code, string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Значение не может быть пустым", nameof(value));
+
+            Code = code;
+            Value = value;
+        }
+    }
+}
---
+++ ITQTestApp.Domain/ValueObjects/Code.cs
@@ -0,0 +1,26 @@
+﻿namespace ITQTestApp.Domain.ValueObjects
+{
+    public sealed class Code : IEquatable<Code>
+    {
+        public int Value { get; }
+
+        public Code(int value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("Код должен быть положительным числом", nameof(value));
+
+            Value = value;
+        }
+
+        public bool Equals(Code? other)
+            => other is not null && Value == other.Value;
+
+        public override bool Equals(object? obj)
+            => obj is Code other && Equals(other);
+
+        public override int GetHashCode()
+            => Value.GetHashCode();
+
+        public static implicit operator int(Code code) => code.Value;
+    }
+}
